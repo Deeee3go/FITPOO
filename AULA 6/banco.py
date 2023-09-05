949f5ad5bd5e161987b4d074267375a7909fe47d
Diego Carvalho <di3go.cs4@gmail.com>
2023-09-04 21:28:45 -0300

    organiza;áo
    organizadas as aulas e arquivos em suas pastas

---
+++ AULA 6/banco.py
@@ -0,0 +1,28 @@
+class Conta:
+    def __init__(self, numero, titular):
+        self.numero = numero
+        self.titular = titular
+        self.__saldo = 0
+
+# metodo acessor
+    @property
+    def saldo(self):
+        return self.__saldo
+
+    def depositar(self, valor):
+        if valor < 0:
+            return 'Por favor, digite um valor positivo'
+        self.__saldo += valor
+        return 'Deposito realziado com sucesso'
+
+    def sacar(self, valor):
+        if valor < 0:
+            return 'Por favor, digite um valor positivo'
+        if valor > self.__saldo:
+            return 'Saldo insuficiente'
+        self.__saldo -= valor
+        return 'Saque realziado com sucesso'
+
+
+if __name__ == '__main__':
+    cli_1 = Conta(1, 'Diego')
---
+++ AULA 8/importacao.py
@@ -0,0 +1,5 @@
+import math
+import math as matematica
+from math import pi, sin, cos
+from math import pi, sin as seno, cos as coseno  
+
